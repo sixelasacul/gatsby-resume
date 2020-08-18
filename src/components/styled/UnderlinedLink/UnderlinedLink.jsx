e3be1596d2ad009b1f14cce69a3033a6c36ef2f5
sixelasacul <alexis.lucas1@viacesi.fr>
2020-08-19 00:17:04 +0200

    Removed the use of FlexContainer in UnderlinedLink

--- src/components/styled/UnderlinedLink/UnderlinedLink.jsx
+++ src/components/styled/UnderlinedLink/UnderlinedLink.jsx
@@ -1,30 +1,27 @@
 import React from "react";
-import styled, { css } from "styled-components";
+import { css } from "styled-components";
 import tw from "twin.macro";
 import { Link } from "gatsby";
 
 import { withChildren } from "../../shapes/withChildren";
 
-const FlexContainer = styled.div`
-	${tw`flex flex-row inline-block`}
-`;
-
 const styles = css`
+	${tw`inline-flex flex-col`}
 	&::after {
 		content: "";
 		width: calc(66% - 0.25rem);
-		${tw`border border-solid border-blue-500 ml-1`}
 		transition-duration: 200ms;
 		transition-name: retractOnMouseOut;
 		transition-timing-function: ease-out;
+		${tw`border border-solid border-blue-500 ml-1`}
 	}
 	&:hover::after,
 	&:focus::after {
 		width: calc(100% - 0.25rem);
-		${tw`border-blue-400`}
 		transition-duration: 200ms;
 		transition-name: extendOnMouseOn;
 		transition-timing-function: ease-in;
+		${tw`border-blue-400`}
 	}
 	@keyframes extendOnMouseOn {
 		from {
@@ -46,31 +43,20 @@ const styles = css`
 			${tw`border-blue-500`}
 		}
 	}
-	display: flex;
-	flex-direction: column;
 `;
 
 export const UnderlinedLink = ({ children, ...otherProps }) => (
-	<FlexContainer>
-		<Link css={styles} {...otherProps}>
-			{children}
-		</Link>
-	</FlexContainer>
+	<Link css={styles} {...otherProps}>
+		{children}
+	</Link>
 );
 
 UnderlinedLink.propTypes = withChildren;
 
 export const UnderlinedExternalLink = ({ children, ...otherProps }) => (
-	<FlexContainer>
-		<a
-			css={styles}
-			target="_blank"
-			rel="noreferrer noopener"
-			{...otherProps}
-		>
-			{children}
-		</a>
-	</FlexContainer>
+	<a css={styles} target="_blank" rel="noreferrer noopener" {...otherProps}>
+		{children}
+	</a>
 );
 
 UnderlinedExternalLink.propTypes = withChildren;
